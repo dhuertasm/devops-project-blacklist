ba3359ba5158ff3b400a44b8df84506b04da6f8e
dhuertasm <dhuertasm@uniandes.edu.co>
2023-04-13 17:43:48 -0500

    github actions test

---
+++ blacklist/tests/test_blacklist.py
@@ -0,0 +1,14 @@
+import json
+from unittest import TestCase
+
+from faker import Faker
+from faker.generator import random
+
+from app import app
+
+
+class TestBlackList(TestCase):
+
+    def setUp(self):
+        pass
+
